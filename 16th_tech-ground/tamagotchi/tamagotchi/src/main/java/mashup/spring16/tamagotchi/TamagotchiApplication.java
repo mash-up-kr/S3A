0b1c1b0c05289df9e92bd220aebb0f0dc31a9c2e
thguss <thgus345@naver.com>
2026-05-16 02:25:27 +0900

    init

---
+++ 16th_tech-ground/tamagotchi/tamagotchi/src/main/java/mashup/spring16/tamagotchi/TamagotchiApplication.java
@@ -0,0 +1,13 @@
+package mashup.spring16.tamagotchi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamagotchiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamagotchiApplication.class, args);
+	}
+
+}
---
+++ 16th_tech-ground/tamagotchi/tamagotchi/src/test/java/mashup/spring16/tamagotchi/TamagotchiApplicationTests.java
@@ -0,0 +1,13 @@
+package mashup.spring16.tamagotchi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TamagotchiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
