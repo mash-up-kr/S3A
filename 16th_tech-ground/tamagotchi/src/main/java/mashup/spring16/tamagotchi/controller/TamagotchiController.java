392320e7a7695e5f9d9d8f774d10b623bb61716e
ej9374 <109258160+ej9374@users.noreply.github.com>
2026-05-28 19:32:39 +0900

    fix: 다마고치 없으면 캐릭터 선택으로 redirect, 한글 이미지 경로 수정

--- 16th_tech-ground/tamagotchi/src/main/java/mashup/spring16/tamagotchi/controller/TamagotchiController.java
+++ 16th_tech-ground/tamagotchi/src/main/java/mashup/spring16/tamagotchi/controller/TamagotchiController.java
@@ -2,10 +2,13 @@ package mashup.spring16.tamagotchi.controller;
 
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import mashup.spring16.tamagotchi.domain.Tamagotchi;
 import mashup.spring16.tamagotchi.repository.TamagotchiRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,10 +22,15 @@ public class TamagotchiController {
         if (memberId == null) {
             return "redirect:/login";
         }
-        tamagotchiRepository.findByMemberId(memberId).ifPresent(t -> {
-            model.addAttribute("character", t.getCharacter());
-            model.addAttribute("petName", t.getName());
-        });
+
+        Optional<Tamagotchi> tamagotchi = tamagotchiRepository.findByMemberId(memberId);
+        if (tamagotchi.isEmpty()) {
+            // 다마고치 없는 경우 캐릭터 선택으로 이동 (session의 memberId는 유지)
+            return "redirect:/signup/select";
+        }
+
+        model.addAttribute("character", tamagotchi.get().getCharacter());
+        model.addAttribute("petName", tamagotchi.get().getName());
         return "tamagotchi";
     }
 }
