e85324555cbd3f7453bc40af55a3224986b54b45
ej9374 <109258160+ej9374@users.noreply.github.com>
2026-05-28 18:41:54 +0900

    feat: MemberRepository, TamagotchiRepository 추가

---
+++ 16th_tech-ground/tamagotchi/src/main/java/mashup/spring16/tamagotchi/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package mashup.spring16.tamagotchi.repository;
+
+import mashup.spring16.tamagotchi.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUsername(String username);
+}
